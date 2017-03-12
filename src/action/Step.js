4ac64081407011a753f7e21f99bd95cdb5fa104d
Amy Ciavolino <amyciav@gmail.com>
2017-03-12 14:03:34 -0400

    Add error message for missing step

--- src/action/Step.js
+++ src/action/Step.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import Menu from './Menu';
 import Action from './Action';
 
+function Error(props) {
+  return <div>
+    <p>Opps something went wrong :(</p>
+    <a onClick={props.onRestart}>Restart?</a>
+  </div>
+}
+
 class Step extends Component {
 
   renderByType() {
+    if (!this.props.data) {
+      return <Error onRestart={this.props.onRestart} />
+    }
+
     switch(this.props.data.type) {
       case 'menu':
         return <Menu
